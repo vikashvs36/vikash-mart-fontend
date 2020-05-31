20584aee6c73bf144c4f740d9381f7ff9c22d8c6
Vikash <vikashvs36@gmail.com>
2020-05-31 19:57:51 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListUserComponent } from './user/list-user/list-user.component';
+import { ViewUserComponent } from './user/view-user/view-user.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
+import { CreateUserComponent } from './user/create-user/create-user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  // {path: 'register', component: RegisterComponent},
+  // {path: '**', component: DashboardComponent}
+
+  {path: 'create-user', component: CreateUserComponent},
+  {path: 'list-user', component: ListUserComponent},
+  {path: 'view-user/:id', component: ViewUserComponent},
+  {path: 'edit-user/:id', component: EditUserComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule  } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListUserComponent } from './user/list-user/list-user.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
+import { ViewUserComponent } from './user/view-user/view-user.component';
+import { CreateUserComponent } from './user/create-user/create-user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ListUserComponent,
+    EditUserComponent,
+    ViewUserComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
